fbeabf39b3d6a1b0c15d94fda9579b6abc90e3b0
primawn <115553484+primawn@users.noreply.github.com>
2022-10-14 20:49:56 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -2,6 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#STREAMLIT
+st.set_page_config(layout="wide")
+#Create header
+st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
+st.write("Prima Widiani | Tetris Program 2022")
+st.write("""## Latar Belakang""")
+st.write("Hidup yang dimudahkan oleh kemajuan teknologi saat ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan."
+         " Menurut WHO, hal-hal tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), dimana penderita diabetes melitus sejauh ini semakin bertambah banyak.")
+st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC."
+           " Tuberkulosis (TBC/TB) sendiri adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
+
+#image
+st.image('./Java_blank_map.jpg')
+
 
 #DATA
 ##JKT
@@ -24,22 +38,6 @@ diy_data = diy_data.rename(columns={'Kabupaten_Kota':'Kabupaten/Kota','TB':'Juml
 banten_dm = pd.read_csv('./Capstone Project/DM_Banten.csv', sep=',')
 banten_dm = banten_dm.rename(columns={'Kabupaten_Kota':'Kabupaten/Kota','Jumlah_Penderita_DM':'Jumlah Penderita DM'})
 banten_dm = banten_dm.drop(labels=['Puskesmas'], axis=1)
-
-
-
-#STREAMLIT
-st.set_page_config(layout="wide")
-#Create header
-st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
-st.write("Prima Widiani | Tetris Program 2022")
-st.write("""## Latar Belakang""")
-st.write("Hidup yang dimudahkan oleh kemajuan teknologi saat ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan."
-         " Menurut WHO, hal-hal tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), dimana penderita diabetes melitus sejauh ini semakin bertambah banyak.")
-st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC."
-           " Tuberkulosis (TBC/TB) sendiri adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
-
-#image
-st.image('./Java_blank_map.jpg')
 
 
 
