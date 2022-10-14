deb61dfe84781958581866bc0b1be43629657441
primawn <115553484+primawn@users.noreply.github.com>
2022-10-14 18:59:47 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -12,7 +12,9 @@ st.write("Hidup yang dimudahkan oleh kemajuan teknologi saat ini membuat kita se
 st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC."
            " Tuberkulosis (TBC/TB) sendiri adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
 
-
+#image
+image = Image.open('https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Java_blank_map.svg/2560px-Java_blank_map.svg.png')
+st.image(image)
 
 st.sidebar.write("Data Penderita Diabetes Melitus dan TBC per Provinsi")
 st.sidebar.caption("Data dihimpun dari website resmi Pemerintah Provinsi di pulau Jawa")
