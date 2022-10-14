3217670931667bda098cfa3e637f1659ab959ff0
primawn <115553484+primawn@users.noreply.github.com>
2022-10-14 18:53:44 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -8,18 +8,10 @@ st.write("""# Prevalensi TBC Terhadap Penderita Diabetes di Pulau Jawa Pada 2020
 st.write("Prima Widiani | Tetris Program 2022")
 st.write("""## Latar Belakang""")
 st.write("Kemajuan teknologi memang membawa pengaruh baik, namun juga membawa dampak buruk bagi manusia. Hidup yang serba mudah membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan."
-         "Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), dimana penderita diabetes melitus sejauh ini semakin bertambah banyak.")
+         " Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), dimana penderita diabetes melitus sejauh ini semakin bertambah banyak.")
 st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC."
-           "Tuberkulosis (TBC/TB) sendiri adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
+           " Tuberkulosis (TBC/TB) sendiri adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
 
-
-
-
-with st.container():
-   st.header("Latar Belakang")
-with st.container():
-   st.write("Kemajuan teknologi memang membawa pengaruh baik, namun juga membawa dampak buruk bagi manusia. Hidup yang serba mudah membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan. Menurut WHO, seperti dikutip oleh Kemkes, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), yang mana sejauh ini penderita diabetes melitus semakin bertambah banyak.")
-   
 
 
 st.sidebar.write("Data Penderita Diabetes Melitus dan TBC per Provinsi")
