e7c911da9a9ca3f7a0a21fb206346691838108a6
primawn <115553484+primawn@users.noreply.github.com>
2022-10-14 21:13:52 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as mp
-from tabulate import tabulate
+import tabulate
 
 #STREAMLIT
 st.set_page_config(layout="wide")
