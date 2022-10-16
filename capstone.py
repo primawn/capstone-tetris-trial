0f0afafc4775502a787233bacb6300e5ba7adb3e
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 00:45:49 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -111,7 +111,7 @@ choice_prov = st.selectbox(
 #displaying the selected option
 st.write('Data penderita DM dan TB di', choice_prov)
 
-if st.write('DKI Jakarta'):
+if st.write=('DKI Jakarta'):
     st.table(jkt_data) #displayed when the button is clicked
 
 #SIDEBAR
