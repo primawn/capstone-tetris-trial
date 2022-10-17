a692ed997236f6b806aa8f032a556d0e87a3c04a
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 20:04:35 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -15,6 +15,16 @@ choice_prov = st.sidebar.selectbox(
     'Data Penderita Diabetes Melitus dan TBC berdasarkan Provinsi',
     ('DKI Jakarta','Banten','DI Yogyakarta','Jawa Barat','Jawa Tengah','Jawa Timur'))
 
+st.sidebar.button('Hit me')
+hide_jkt_data_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
+st.markdown(hide_jkt_data_row_index, unsafe_allow_html=True)
+jawa_data.sort_values(by=['Kabupaten/Kota'], inplace=True, ascending=True)
+
 if choice_prov == "DKI Jakarta":
          st.sidebar.table(data.jkt_data)
 if choice_prov == "Banten":
@@ -29,6 +39,8 @@ if choice_prov == "Jawa Timur":
          st.sidebar.table(data.jatim_data)
          
 st.sidebar.caption("(Data dihimpun dari website resmi Pemerintah Provinsi di pulau Jawa)")
+
+
 
 #Create header
 st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
