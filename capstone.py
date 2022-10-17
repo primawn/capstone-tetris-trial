f3b3a66a604d47473d152817112c83dc78594767
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 19:57:33 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -37,22 +37,6 @@ st.write("Prima Widiani | Tetris Program 2022")
 #image
 st.image('./Java_blank_map.jpg')
 
-#Sidebar
-navigation = st.sidebar.selectbox(
-    'Navigasi',
-    ('Judul','Latar Belakang','Data per Provinsi','Jawa Barat','Jawa Tengah','Jawa Timur'))
 
-if navigation == "Judul":
-         st.sidebar.table(jkt_data)
-if navigation == "Banten":
-         st.sidebar.table(banten_data)
-if navigation == "DI Yogyakarta":
-         st.sidebar.table(diy_data)
-if navigation == "Jawa Barat":
-         st.sidebar.table(jabar_data)
-if navigation == "Jawa Tengah":
-         st.sidebar.table(jateng_data)
-if navigation == "Jawa Timur":
-         st.sidebar.table(jatim_data)
          
 
