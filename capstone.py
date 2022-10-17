13716d83544951eca707e70b6de8fe0785b68317
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 20:22:35 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -10,6 +10,13 @@ import data
 
 
 #STREAMLIT
+#Create header
+st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
+st.write("Prima Widiani | Tetris Program 2022")
+
+#image
+st.image('./Java_blank_map.jpg')
+
 st.write("""Latar Belakang""")
 st.write("Hidup yang menjadi lebih mudah di kemajuan teknologi ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan. Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), yang mana sejauh ini penderita diabetes melitus semakin bertambah banyak.")
 st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC. Tuberkulosis (TBC/TB) adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
@@ -36,12 +43,7 @@ st.sidebar.caption("(Data dihimpun dari website resmi Pemerintah Provinsi di pul
 
 
 
-#Create header
-st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
-st.write("Prima Widiani | Tetris Program 2022")
 
-#image
-st.image('./Java_blank_map.jpg')
 
 
          
