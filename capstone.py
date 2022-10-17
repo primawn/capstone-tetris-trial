b5729b29f332e6e39f200b3803d0513541744b31
prasetyon <prstyngrhd@gmail.com>
2022-10-18 01:57:45 +0700

    Add description

--- capstone.py
+++ capstone.py
@@ -11,6 +11,14 @@ import map
 st.write("""# Hubungan Prevalensi Diabetes Melitus dengan Prevalensi TB Paru di Pulau Jawa Tahun 2020""")
 st.write("Prima Widiani | Tetris Program 2022")
 
+map.show_maps(map.select_data, map.threshold(map.select_data))
+st.markdown("<h6 style='text-align: center;'>Persebaran "+map.select_data+" di Pulau Jawa</h6>", unsafe_allow_html=True)
+
+# st.write("""Latar Belakang""")
+st.write("Hidup yang menjadi lebih mudah di kemajuan teknologi ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan. Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), yang mana sejauh ini penderita diabetes melitus semakin bertambah banyak.")
+st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC. Tuberkulosis (TBC/TB) adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
+
+
 # df = pd.DataFrame(data.datajawa)
 # df = df.set_index('Provinsi')
 chart_data = pd.DataFrame(
@@ -20,6 +28,10 @@ chart_data = pd.DataFrame(
 )
 
 st.bar_chart(chart_data)
+st.markdown("<h6 style='text-align: center;'>Jumlah Penderita Tuberkulosis dan Diabetes di Pulau Jawa</h6>", unsafe_allow_html=True)
+
+st.write("Bila dilihat pada diagram batang di atas, maka hubungan antara data penderita kedua penyakit tersebut adalah tidak berhubungan.")
+st.write("Meskipun memiliki jumlah penderita Diabetes tertinggi, namun jumlah penderita Tuberkulosis di Jawa Barat masih lebih sedikit daripada di Jawa Tengah yang merupakan provinsi dengan jumlah penderita Tuberkulosis tertinggi di Pulau Jawa.")
 
 # data = pd.melt(chart_data.reset_index(), id_vars=["index"])
 # chart = (
@@ -37,10 +49,6 @@ st.bar_chart(chart_data)
 #image
 # st.image('./Java_blank_map.jpg')
 # st.write(data_all['Provinsi'])
-
-# st.write("""Latar Belakang""")
-st.write("Hidup yang menjadi lebih mudah di kemajuan teknologi ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan. Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), yang mana sejauh ini penderita diabetes melitus semakin bertambah banyak.")
-st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC. Tuberkulosis (TBC/TB) adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
 
 # st.table(data.datajawa)
 
@@ -66,9 +74,6 @@ st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan 
 # ).configure_view(stroke='transparent')
 
 # chart.display() 
-
-st.write("Persebaran "+map.select_data+' di Pulau Jawa')
-map.show_maps(map.select_data, map.threshold(map.select_data))
 
 choice_prov = st.sidebar.selectbox(
     'Data Penderita Diabetes Melitus dan TBC berdasarkan Provinsi',
