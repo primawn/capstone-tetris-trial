514ffaedc1e4a8c3b4c5b1016bbefac7d52c7fde
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 13:43:13 +0700

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def app():
-    st.title('Latar Belakang')
-    st.write('Latar Belakang')
+    st.title("Latar Belakang")
+    st.write("Latar Belakang")
     
 
