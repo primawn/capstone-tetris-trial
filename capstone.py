affc68784da1e970b8fbf59f76df7425c3fbb3c8
primawn <115553484+primawn@users.noreply.github.com>
2022-10-17 21:18:15 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -21,6 +21,18 @@ st.write("""Latar Belakang""")
 st.write("Hidup yang menjadi lebih mudah di kemajuan teknologi ini membuat kita seringkali menjadi lalai dalam menjaga gaya hidup dan pola makan. Menurut WHO, gaya hidup yang tidak baik tersebut merupakan salah satu pemicu diabetes melitus (Kemkes, 2018), yang mana sejauh ini penderita diabetes melitus semakin bertambah banyak.")
 st.caption("Diabetes melitus merupakan suatu penyakit yang menyebabkan gangguan metabolisme kronis pada tubuh (WHO, 1999). Hal ini menyebabkan tingginya kadar gula darah, terganggunya produksi insulin dalam tubuh, dan seringkali juga memberikan peluang bagi penyakit lain untuk masuk ke tubuh kita, salah satunya adalah TBC. Tuberkulosis (TBC/TB) adalah penyakit mudah menular yang disebabkan oleh infeksi bakteri, yang pada umumnya menyerang paru-paru.")
 
+#chart
+alt.data_transformers.disable_max_rows()
+
+np.random.seed(0)
+data = pd.DataFrame({
+    'Jumlah': pd.date_range('1990-01-01', freq='Y', periods=10),
+    'FAO_yied': np.random.randn(10).cumsum()
+    'Simulation': np.random.randn(10).cumsum()
+    'Predicted': np.random.randn(10).cumsum()
+})
+
+data_jawa = pd.melt(data, id_vars=['date'], value_vars=['FAO_yied', 'Predicted', 'Simulation'])
 chart = alt.Chart(data.datajawa, title='Jumlah penderita DM dan TB di Pulau Jawa 2020 berdasarkan Provinsi').mark_bar(
     opacity=1,
     ).encode(
